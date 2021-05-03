9d2afcb34968bcb36cf1d4f3cfae446ebdad0c2d
Prathamesh Deshpande <prtdesh25@gmail.com>
2021-05-03 13:40:01 +0530

    Model: Auto Encoder Added

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from imports import *
 from model import create_model,create_features
 
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+   tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
 # Taking images from dataset
 path = "/home/prathamesh/Desktop/ML_Projects/Auto_Colorization/mirflickr25k"
 train_datagen = ImageDataGenerator(rescale=1. / 255)
@@ -58,3 +63,21 @@ callbacks_list = [checkpoint]
 model.fit(vgg_features, Y,validation_split=0.2, verbose=1, epochs=100, batch_size=32,callbacks=callbacks_list)
 
 
+
+test_path = "/home/prathamesh/Desktop/ML_Projects/Auto_Colorization/mirflickr25k/mirflickr/im3.jpg"
+test = img_to_array(load_img(test_path))
+test = resize(test, (224,224), anti_aliasing=True)
+test*= 1.0/255
+lab = rgb2lab(test)
+l = lab[:,:,0]
+L = gray2rgb(l)
+L = L.reshape((1,224,224,3))
+#print(L.shape)
+vggpred = newmodel.predict(L)
+ab = model.predict(vggpred)
+#print(ab.shape)
+ab = ab*128
+cur = np.zeros((224, 224, 3))
+cur[:,:,0] = l
+cur[:,:,1:] = ab
+imsave("result.jpg", lab2rgb(cur))
